cb84aa1f9fa6afd632c6e62f783ddad8da25a53f
egirsova <liza@girsovs.com>
2015-04-26 18:56:38 +0100

    Final Version

--- TravelingSalesman_GBFS_h1/main.cpp
+++ TravelingSalesman_GBFS_h1/main.cpp
@@ -7,12 +7,29 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <string>
+#include <map>
+#include "Domain.h"
+#include "GBFS.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<"Algorithm: Greedy Best-First Search"<<endl;
+    cout<<"Domain: Traveling Salesman Problem"<<endl<<endl;
+    cout<<"Enter starting location: ";
+    string start;
+    getline(cin, start);
+    
+    Domain d;
+    GBFS newGBFS;
+    newGBFS.runGBFS(d, start, start);
+    
     return 0;
 }
+
 
